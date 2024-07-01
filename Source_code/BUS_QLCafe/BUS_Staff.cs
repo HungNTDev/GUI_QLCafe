25b245ee3c11d424cc6f007461ce95e354c72526
Hexstars <baotnc19@gmail.com>
2024-07-01 16:48:04 +0700

    Thêm các phương thức trong BUS_Staff, DAL_Staff

--- Source_code/BUS_QLCafe/BUS_Staff.cs
+++ Source_code/BUS_QLCafe/BUS_Staff.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using DAL_QLCafe;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,6 +10,66 @@ namespace BUS_QLCafe
 {
     public class BUS_Staff
     {
+        DAL_Staff dalStaff = new DAL_Staff();
+        //xử lí md5
+        public string encryption(string password)
+        {
+            MD5CryptoServiceProvider md5 = new MD5CryptoServiceProvider();
+            byte[] encrypt;
+            UTF8Encoding encoding = new UTF8Encoding();
+            //encrypt the given password string into Encrypted Data
+            encrypt = md5.ComputeHash(encoding.GetBytes(password));
+            StringBuilder encryptdata = new StringBuilder();
+            //Create new string by using encryted data
+            for (int i = 0; i < encrypt.Length; i++)
+            {
+                encryptdata.Append(encrypt[i].ToString());
+            }
+            return encryptdata.ToString();
+        }
+        public void DangNhap()
+        {
 
+        }
+        public void QuenMK()
+        {
+
+        }
+        public void CapNhatMK()
+        {
+
+        }
+        public void VaiTro()
+        {
+
+        }
+        public void LayMK()
+        {
+
+        }
+        public void GetID()
+        {
+
+        }
+        public void get()
+        {
+
+        }
+        public void insert()
+        {
+
+        }
+        public void update()
+        {
+
+        }
+        public void delete()
+        {
+
+        }
+        public void search()
+        {
+
+        }
     }
 }
--- Source_code/DAL_QLCafe/DAL_Staff.cs
+++ Source_code/DAL_QLCafe/DAL_Staff.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using DTO_QLCafe;
+using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +11,53 @@ namespace DAL_QLCafe
 {
     public class DAL_Staff
     {
+        public void dangNhap(DTO_Staff staff)
+        {
+
+        }
+        public void quenMK(string email)
+        {
+
+        }
+        public void layMK(string email)
+        {
+
+        }
+        public void capNhatMK(string email, string matkhau)
+        {
+
+        }
+        public void vaiTro(string email)
+        {
+
+        }
+        public void getID()
+        {
+
+        }
+        public void checkEmail(string email)
+        {
+
+        }
+        public void get()
+        {
+
+        }
+        public void insert()
+        {
+
+        }
+        public void update()
+        {
+
+        }
+        public void delete() 
+        {
+
+        }
+        public void search()
+        {
+
+        }
     }
 }

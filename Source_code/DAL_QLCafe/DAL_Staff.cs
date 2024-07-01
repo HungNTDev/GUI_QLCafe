301f2951717975783ae0e640bfbbd231ab30bc44
HungNTDev <kmac08552@gmail.com>
2024-07-01 20:41:49 +0700

    Xóa form mặc định

--- Source_code/DAL_QLCafe/DAL_Staff.cs
+++ Source_code/DAL_QLCafe/DAL_Staff.cs
@@ -1,13 +1,7 @@
 ﻿using DTO_QLCafe;
 using System;
-using System.Collections;
-using System.Collections.Generic;
 using System.Data;
 using System.Data.SqlClient;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Xml.Linq;
 
 namespace DAL_QLCafe
 {
--- Source_code/GUI_QLCafe/Program.cs
+++ Source_code/GUI_QLCafe/Program.cs
@@ -1,7 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace GUI_QLCafe
@@ -16,7 +13,7 @@ namespace GUI_QLCafe
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new frmDangNhap());
         }
     }
 }

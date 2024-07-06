c95f777830cc7f78958b35a2537a3228f0d7198d
HungNTDev <kmac08552@gmail.com>
2024-07-07 00:27:39 +0700

    Code Bus dal dto TableCF

---
+++ Source_code/DAL_QLCafe/DAL_TableCF.cs
@@ -0,0 +1,150 @@
+﻿using DTO_QLCafe;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace DAL_QLCafe
+{
+    public class DAL_TableCF : DBConnect
+    {
+        public DataTable get()
+        {
+            try
+            {
+                using (SqlCommand cmd = new SqlCommand("GetTableCF", conn))
+                {
+                    cmd.CommandType = CommandType.StoredProcedure;
+
+                    conn.Open();
+
+                    DataTable dtTableCF = new DataTable();
+                    dtTableCF.Load(cmd.ExecuteReader());
+                    return dtTableCF;
+                }
+            }
+            finally
+            {
+                if (conn.State == ConnectionState.Open)
+                {
+                    conn.Close();
+                }
+            }
+        }
+
+        public bool insert(DTO_TableCF obj)
+        {
+            try
+            {
+                using (SqlCommand cmd = new SqlCommand("InsertTableCF", conn))
+                {
+                    cmd.CommandType = CommandType.StoredProcedure;
+
+                    cmd.Parameters.AddWithValue("@IdTable", obj.IdTable);
+                    cmd.Parameters.AddWithValue("@NameTable", obj.NameTable);
+                    cmd.Parameters.AddWithValue("@StatusTable", obj.StatusTable);
+
+                    conn.Open();
+
+                    if (cmd.ExecuteNonQuery() > 0)
+                    {
+                        return true;
+                    }
+                }
+            }
+            finally
+            {
+                if (conn.State == ConnectionState.Open)
+                {
+                    conn.Close();
+                }
+            }
+            return false;
+        }
+
+        public bool update(DTO_TableCF obj)
+        {
+            try
+            {
+                using (SqlCommand cmd = new SqlCommand("UpdateTableCF", conn))
+                {
+                    cmd.CommandType = CommandType.StoredProcedure;
+
+                    cmd.Parameters.AddWithValue("@IdTable", obj.IdTable);
+                    cmd.Parameters.AddWithValue("@NameTable", obj.NameTable);
+                    cmd.Parameters.AddWithValue("@StatusTable", obj.StatusTable);
+
+                    conn.Open();
+
+                    if (cmd.ExecuteNonQuery() > 0)
+                    {
+                        return true;
+                    }
+                }
+            }
+            finally
+            {
+                if (conn.State == ConnectionState.Open)
+                {
+                    conn.Close();
+                }
+            }
+            return false;
+        }
+
+        public bool delete(DTO_TableCF obj)
+        {
+            try
+            {
+                using (SqlCommand cmd = new SqlCommand("DeleteTableCF", conn))
+                {
+                    cmd.CommandType = CommandType.StoredProcedure;
+
+                    cmd.Parameters.AddWithValue("@IdTable", obj.IdTable);
+                    cmd.Parameters.AddWithValue("@NameTable", obj.NameTable);
+                    cmd.Parameters.AddWithValue("@StatusTable", obj.StatusTable);
+
+                    conn.Open();
+
+                    if (cmd.ExecuteNonQuery() > 0)
+                    {
+                        return true;
+                    }
+                }
+            }
+            finally
+            {
+                if (conn.State == ConnectionState.Open)
+                {
+                    conn.Close();
+                }
+            }
+            return false;
+        }
+
+        public DataTable search(string keyword, string column)
+        {
+            try
+            {
+                using (SqlCommand cmd = new SqlCommand("SearchTableCF", conn))
+                {
+                    cmd.CommandType = CommandType.StoredProcedure;
+
+                    cmd.Parameters.AddWithValue("@keyword", keyword);
+                    cmd.Parameters.AddWithValue("@column", column);
+
+                    conn.Open();
+
+                    DataTable dtTableCF = new DataTable();
+                    dtTableCF.Load(cmd.ExecuteReader());
+                    return dtTableCF;
+                }
+            }
+            finally
+            {
+                if (conn.State == ConnectionState.Open)
+                {
+                    conn.Close();
+                }
+            }
+        }
+    }
+}
---
+++ Source_code/DTO_QLCafe/DTO_TableCF.cs
@@ -0,0 +1,23 @@
+﻿namespace DTO_QLCafe
+{
+    public class DTO_TableCF
+    {
+        public string idTable { get; set; }
+        public string nameTable { get; set; }
+        public int statusTable { get; set; }
+
+        public string IdTable { get => idTable; set => idTable = value; }
+
+        public string NameTable { get => nameTable; set => nameTable = value; }
+
+        public int StatusTable { get => statusTable; set => statusTable = value; }
+
+        public DTO_TableCF() { }
+        public DTO_TableCF(string idTable, string nameTable, int statusTable)
+        {
+            this.idTable = idTable;
+            this.nameTable = nameTable;
+            this.statusTable = statusTable;
+        }
+    }
+}

74ebeea92a7ef0d1cf9fc1e434ed197c67e24657
HungNTDev <kmac08552@gmail.com>
2024-07-19 17:52:33 +0700

    Hùng

--- Source_code/GUI_QLCafe/Form/frmDangNhap.Designer.cs
+++ Source_code/GUI_QLCafe/Form/frmDangNhap.Designer.cs
@@ -374,6 +374,7 @@
             // 
             // frmDangNhap
             // 
+            this.AcceptButton = this.btnDN;
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1171, 684);

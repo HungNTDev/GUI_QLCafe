badbd3e014be6139deb38957aa9267515a0df901
Hexstars <baotnc19@gmail.com>
2025-02-16 16:52:28 +0700

    Thêm project reference

--- Source_code/NUnit Tests/Staff.cs
+++ Source_code/NUnit Tests/Staff.cs
@@ -1,10 +1,14 @@
-﻿namespace NUnit_Tests
+﻿using DAL_QLCafe;
+
+namespace NUnit_Tests
 {
     public class Staff
     {
+        private DAL_Staff staff;
         [SetUp]
         public void Setup()
         {
+            staff = new DAL_Staff();
         }
 
         [Test]

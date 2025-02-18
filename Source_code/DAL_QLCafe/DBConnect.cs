1939b1f9d6da1cffeb006f2f4d258c11104da1c8
Hexstars <baotnc19@gmail.com>
2025-02-18 20:45:25 +0700

    Sửa

--- Source_code/DAL_QLCafe/DBConnect.cs
+++ Source_code/DAL_QLCafe/DBConnect.cs
@@ -5,6 +5,6 @@ namespace DAL_QLCafe
     public class DBConnect
     {
         //protected SqlConnection conn = new SqlConnection("Data source=DESKTOP-QIE438C\\MAYAO; database=QL_Cafe; integrated security=true");
-        protected string _conn = "Data source=DESKTOP-QIE438C\\MAYAO;Initial Catalog = QL_Cafe; Integrated Security = True";
+        protected string _conn = "Data source=CHIBAO\\SQLEXPRESS;Initial Catalog = QL_Cafe; Integrated Security = True";
     }
 }

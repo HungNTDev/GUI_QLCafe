daee0394d858b1a70f8ec4613eaacf598f6982ef
Mai Đức Anh Tuấn <maiducanhtuan14@gmail.com>
2025-02-25 15:32:29 +0700

    Đổi mật khẩu

--- Source_code/DAL_QLCafe/DBConnect.cs
+++ Source_code/DAL_QLCafe/DBConnect.cs
@@ -1,10 +1,12 @@
-﻿using System.Web.Configuration;
+﻿using System.Drawing;
+using System.Web.Configuration;
 
 namespace DAL_QLCafe
 {
     public class DBConnect
     {
         //protected SqlConnection conn = new SqlConnection("Data source=DESKTOP-QIE438C\\MAYAO; database=QL_Cafe; integrated security=true");
-        protected string _conn = "Data source=CHIBAO\\SQLEXPRESS;Initial Catalog = QL_Cafe; Integrated Security = True";
+        protected string _conn = "Data Source=MSI\\MAYAO;Initial Catalog = QL_Cafe; Integrated Security = True; Trust Server Certificate=True";
     }
 }
+
---
+++ Source_code/NUnit Tests/ChangePassword.cs
@@ -0,0 +1,57 @@
+﻿using DAL_QLCafe;
+using DTO_QLCafe;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace NUnit_Tests
+{
+    public class ChangePasswordTests
+    {
+        private DAL_Staff staffDAL;
+
+        [SetUp]
+        public void Setup()
+        {
+            staffDAL = new DAL_Staff();
+        }
+
+        [Test]
+        public void ChangePasswordWithEmptyOldPassword()
+        {
+           
+            bool result = staffDAL.UpdateMK("hungntps38090@gmail.com", "", "123");
+            Assert.IsFalse(result, "Change password should fail when old password is empty.");
+        }
+
+        [Test]
+        public void ChangePasswordWithEmptyNewPassword()
+        {
+            bool result = staffDAL.UpdateMK("hungntps38090@gmail.com", "1234", "");
+            Assert.IsFalse(result, "Change password should fail when new password is empty.");
+        }
+
+        [Test]
+        public void ChangePasswordWithIncorrectOldPassword()
+        {
+            bool result = staffDAL.UpdateMK("hungntps38090@gmail.com", "123", "1234");
+            Assert.IsFalse(result, "Change password should fail when old password is incorrect.");
+        }
+
+        [Test]
+        public void ChangePasswordSuccessfully()
+        {
+            bool result = staffDAL.UpdateMK("hungntps38090@gmail.com", "1234", "123456");
+            Assert.IsTrue(result, "Change password should succeed with correct old password and valid new password.");
+        }
+
+        [TearDown]
+        public void Cleanup()
+        {
+            // No cleanup needed for change password tests
+        }
+    }
+}
